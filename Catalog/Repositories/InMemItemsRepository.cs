40f9284dbf3794e5e8425e25f3fdb9c55a52398d
Alin Apostol <alin.apostol@starbyte.ro>
2022-01-04 12:19:57 +0200

    Faking a DB with a in-mem repository

---
+++ Catalog/Repositories/InMemItemsRepository.cs
@@ -0,0 +1,20 @@
+﻿using Catalog.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.Repositories
+{
+    public class InMemItemsRepository
+    {
+        private readonly List<Item> _items = new()
+        {
+            new Item { Id = Guid.NewGuid(), Name = "Potion", Price = 9, CreatedDate = DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "Iron Sword", Price = 20, CreatedDate = DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "Bronze Shield", Price = 18, CreatedDate = DateTimeOffset.UtcNow },
+        };
+
+
+    }
+}
