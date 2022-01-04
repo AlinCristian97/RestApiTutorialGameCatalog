c9594c66552eab0d5eb058ec18d8a458911bf821
Alin Apostol <alin.apostol@starbyte.ro>
2022-01-04 12:15:05 +0200

    Created 'entities' folder and added first entity (Item)

---
+++ Catalog/Entities/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.Entities
+{
+    public record Item // 'record' is special type of class with support for immutable objects
+    {
+        public Guid Id { get; init; } // 'init' allows setting the property only during initialization
+        public string Name { get; init; }
+        public decimal Price { get; init; }
+        public DateTimeOffset CreatedDate { get; init; }
+    }
+}
