b5db1b50e74e29267509e1eec2558dc8c8c38a15
Alin Apostol <alin.apostol@starbyte.ro>
2022-01-05 10:21:55 +0200

    Added MongoDB.Driver nuget package and created new concrete implementation of IItemsRepository for MongoDB

---
+++ Catalog/Repositories/MongoDbItemsRepository.cs
@@ -0,0 +1,41 @@
+﻿using Catalog.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.Repositories
+{
+    public class MongoDbItemsRepository : IItemsRepository
+    {
+        public MongoDbItemsRepository()
+        {
+
+        }
+
+        public void CreateItem(Item item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteItem(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Item GetItem(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Item> GetItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateItem(Item item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
