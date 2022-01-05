ee4333376d094ce280e784e8eb898bc6f3fa978b
Alin Apostol <alin.apostol@starbyte.ro>
2022-01-05 14:03:01 +0200

    re-commented InMemItemsRepo

--- Catalog/Repositories/InMemItemsRepository.cs
+++ Catalog/Repositories/InMemItemsRepository.cs
@@ -1,45 +1,45 @@
-﻿using Catalog.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿//using Catalog.Entities;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
 
-namespace Catalog.Repositories
-{
-    public class InMemItemsRepository : IItemsRepository
-    {
-        private readonly List<Item> _items = new()
-        {
-            new Item { Id = Guid.NewGuid(), Name = "Potion", Price = 9, CreatedDate = DateTimeOffset.UtcNow },
-            new Item { Id = Guid.NewGuid(), Name = "Iron Sword", Price = 20, CreatedDate = DateTimeOffset.UtcNow },
-            new Item { Id = Guid.NewGuid(), Name = "Bronze Shield", Price = 18, CreatedDate = DateTimeOffset.UtcNow },
-        };
+//namespace Catalog.Repositories
+//{
+//    public class InMemItemsRepository : IItemsRepository
+//    {
+//        private readonly List<Item> _items = new()
+//        {
+//            new Item { Id = Guid.NewGuid(), Name = "Potion", Price = 9, CreatedDate = DateTimeOffset.UtcNow },
+//            new Item { Id = Guid.NewGuid(), Name = "Iron Sword", Price = 20, CreatedDate = DateTimeOffset.UtcNow },
+//            new Item { Id = Guid.NewGuid(), Name = "Bronze Shield", Price = 18, CreatedDate = DateTimeOffset.UtcNow },
+//        };
 
-        public async Task<IEnumerable<Item>> GetItemsAsync()
-        {
-            return await Task.FromResult(_items);
-        }
+//        public async Task<IEnumerable<Item>> GetItemsAsync()
+//        {
+//            return await Task.FromResult(_items);
+//        }
 
-        public Item GetItemAsync(Guid id)
-        {
-            return _items.Where(item => item.Id == id).SingleOrDefault();
-        }
+//        public Item GetItemAsync(Guid id)
+//        {
+//            return _items.Where(item => item.Id == id).SingleOrDefault();
+//        }
 
-        public void CreateItemAsync(Item item)
-        {
-            _items.Add(item);
-        }
+//        public void CreateItemAsync(Item item)
+//        {
+//            _items.Add(item);
+//        }
 
-        public void UpdateItemAsync(Item item)
-        {
-            var index = _items.FindIndex(existingItem => existingItem.Id == item.Id);
-            _items[index] = item;
-        }
+//        public void UpdateItemAsync(Item item)
+//        {
+//            var index = _items.FindIndex(existingItem => existingItem.Id == item.Id);
+//            _items[index] = item;
+//        }
 
-        public void DeleteItemAsync(Guid id)
-        {
-            var index = _items.FindIndex(existingItem => existingItem.Id == id);
-            _items.RemoveAt(index);
-        }
-    }
-}
+//        public void DeleteItemAsync(Guid id)
+//        {
+//            var index = _items.FindIndex(existingItem => existingItem.Id == id);
+//            _items.RemoveAt(index);
+//        }
+//    }
+//}
